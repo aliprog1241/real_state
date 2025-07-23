d6e59205c0a04c22d7b46f6eee50995804a28a73
Ali Miri <miri03369@gmail.com>
2025-07-23 17:42:55 +0330

    creat and print information of customer

--- base.py
+++ base.py
@@ -1,4 +1,27 @@
+#import ABC for easy OOP
+
 from abc import ABC 
 
-class BaseClass(ABC):
-    pass
+
+import uuid
+
+#creat BasseClass
+
+class BaseClass:
+    def __init__(self):
+        self.id = self.generate_id()
+
+    def generate_id(self):
+        return str(uuid.uuid4()) 
+
+@classmethod
+
+def generate_id(cls):
+    cls._id += 1
+    return cls._id
+
+
+@classmethod
+def store (cls, obj):
+    cls.object_list.append(obj)
+    
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hi')
+from random import choice
+from user import User
+
+#creat information for customer
+
+FIRST_NAME = ['Ali', 'Reza', 'mehdi']
+LAST_NAME = ['Miri', 'Rezvani', 'Mosavi']
+MOBILES = ['09199983035', '09196696504', '09159228564', '09128464401', '09121112334']
+
+#prints information for customer
+if __name__ =="__main__":
+    for mobile in MOBILES:
+        User (choice(FIRST_NAME), choice(LAST_NAME), mobile)
+    for user in User.object_list:
+        print(f"{user.id}\t {user.fullname}")
